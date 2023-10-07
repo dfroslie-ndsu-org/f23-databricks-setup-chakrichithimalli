34f7d744df91d5fa73173d8cbc321bd63bd1c0ba
chakrichithimalli <chakrichithimalli>
2023-10-07 23:15:25 +0000

    Databricks Assignments

--- SetupAssignment.py
+++ SetupAssignment.py
@@ -1,14 +1,4 @@
 # Databricks notebook source
-# MAGIC %md
-# MAGIC # Databricks Setup Assignment
-# MAGIC This notebook is the basis for the execution portion of the Databricks setup assignment.  This will involve connecting to your storage account to execute a couple of pre-written basic commands.  Then you will a simple column add of your own and save the results.
-
-# COMMAND ----------
-
-# The first step is to connect to your storage account after setting up a Databricks secret scope and a key vault.  Follow the PDF walkthrough to get this set up.
-# You can use the storage account from the first assignment (GitHub and Azure) for this assignment.
-
-# You'll need to replace these values with the specifics for your setup.
 
 storage_end_point = "chakristorageaccount.dfs.core.windows.net" 
 my_scope = "Chakri-ADB"
@@ -21,42 +11,25 @@ spark.conf.set(
 # Replace the container name (assign-1-blob) and storage account name (assign1storage) in the uri.
 uri = "abfss://chakricontainer@chakristorageaccount.dfs.core.windows.net/"
 
-# COMMAND ----------
 
 # Read the data file from the storage account.  This the same datafile used in assignment 1.  It is also available in the InputData folder of this assignment's repo.
 sp_df = spark.read.csv(uri+'SandP500Daily.csv', header=True)
- 
-display(sp_df)
 
-# COMMAND ----------
-
-# Create new column with the range for the day.
+# Creating a new column with the range for the day.
 sp_range_df = sp_df.withColumn('Range', sp_df.High - sp_df.Low)
-
-display(sp_range_df)
-
-# COMMAND ----------
 
 # Save this range file to a single CSV.  Use coalesce to output it to a single file.
 sp_range_df.coalesce(1).write.option('header',True).mode('overwrite').csv(uri+"output/Range")
 
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC ### Your coding part starts here.  
-# MAGIC Fill in the PySpark in the following notebook cells.
-# MAGIC
-
-# COMMAND ----------
-
 # Use the range from the previous cells to find the percent change for each day.  Use the Open column for the denominator.
-# Sort the dataset descending based on the percent change (High to Low).
+# Percent change calculation using Range, Open values
+percent_value = (sp_range_df.Range/sp_range_df.Open) * 100
 
+# Creating column with percent change value
+sp_range_df_percent_change = sp_range_df.withColumn('Percent change', percent_value )
 
+# Sorting the dataset descending based on the percent change (High to Low).
+sorted_df_percent_change=sp_range_df_percent_change.orderBy('Percent Change', ascending = False)
 
-# COMMAND ----------
-
-# Save the file to a single CSV file to your storage account to a single CSV file in the location output/PercentChange.
-
-
+# Saving the file to a single CSV file to your storage account to a single CSV file in the location output/PercentChange.
+sorted_df_percent_change.coalesce(1).write.option('header',True).mode('overwrite').csv(uri+"output/PercentChange")
